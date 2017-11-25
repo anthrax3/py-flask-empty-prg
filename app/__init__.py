85099efa6842fac4eb8e3f22cd9e4b9cc3179b0b
oilnur <ilnursoft@gmail.com>
2017-11-25 18:54:20 +0300

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, Blueprint
 from flask_mail import Mail
-from .main import main as main_blueprint
 from config import config
 
 mail = Mail()
@@ -14,7 +13,7 @@ def create_app(config_name):
     mail.init_app(app)
 
     # здесь подключаются маршруты и нестандартные страницы с сообщениями об ошибках
-    
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)   # регистрация макета
 
     return app
